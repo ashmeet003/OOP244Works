97bc1f6fe96d4ba25b9cc0af3e92171b7d8f7c33
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2022-03-07 21:13:36 -0500

    Part2 updated

--- Workshop6Part2/Name.cpp
+++ Workshop6Part2/Name.cpp
@@ -2,71 +2,282 @@
 Name: Ashmeet Kaur
 Seneca email: ashmeet-kaur1@myseneca.ca
 Student Id: 122421217
-Date: March 06, 2022
+Date: March 07, 2022
 I have done all the coding by myself and only copied the code that my professor provided to
  complete my workshops and assignments.
 */
 #define _CRT_SECURE_NO_WARNINGS
 #include "Name.h"
-#include <cstring>
-namespace sdds {
+using namespace std;
+namespace sdds
+{
+    const int MAX_NAME_LEN = 20;
+    bool Name::checkSpace(const char* str)
+    {
+        bool result = true;
+        while (*str != '\0') {
+            if (!isspace(*str)) {
+                result = false;
+                break;
+            }
+            str++;
+        }
+        return result;
+    }
+    Name::Name()
+    {
+        fname = nullptr;
+        mname = nullptr;
+        lname = nullptr;
+        abbereviation = false;
+    }
 
-	void sdds::Name::setEmpty()
-	{
-		if (m_fname != nullptr) delete[] m_fname;
-		if (m_mname != nullptr) delete[] m_mname;
-		if (m_lname != nullptr) delete[] m_lname;
-		char* m_fname = nullptr;
-		char* m_mname = nullptr;
-		char* m_lname = nullptr;
-	}
+    Name::Name(const char* fname, const char* mname, const char* lname)
+    {
+        if (fname == nullptr || mname == nullptr || lname == nullptr)
+            setEmpty();
+        else{
+            if (!checkSpace(fname)){
+                this->fname = new char[strlen(fname) + 1];
+                strcpy(this->fname, fname);
+            }
+            else
+                this->fname = nullptr;
+            if (!checkSpace(mname)){
+                this->mname = new char[strlen(mname) + 1];
+                strcpy(this->mname, mname);
+            }
+            else
+                this->mname = nullptr;
+            if (!checkSpace(lname)){
+                this->lname = new char[strlen(lname) + 1];
+                strcpy(this->lname, lname);
+            }
+            else
+                this->lname = nullptr;
+            abbereviation = false;
+        }
+    }
+    
+    Name::Name(const Name& N)
+    {
+        if (N.fname != nullptr){
+            fname = new char[strlen(N.fname) + 1];
+            strcpy(fname, N.fname);
+        }
+        else
+            fname = nullptr;
+        if (N.mname != nullptr){
+            mname = new char[strlen(N.mname) + 1];
+            strcpy(mname, N.mname);
+        }
+        else
+            mname = nullptr;
+        if (N.lname != nullptr){
+            lname = new char[strlen(N.lname) + 1];
+            strcpy(lname, N.lname);
+        }
+        else
+            lname = nullptr;
+        abbereviation = N.abbereviation;
+    }
+    
+    Name::Name(const char* fname, const char* lname)
+    {
+        if (fname == nullptr || lname == nullptr)
+            setEmpty();
+        else{
+            if (!checkSpace(fname)){
+                this->fname = new char[strlen(fname) + 1];
+                strcpy(this->fname, fname);
+            }
+            else
+                this->fname = nullptr;
+            this->mname = nullptr;
+            if (!checkSpace(lname)){
+                this->lname = new char[strlen(lname) + 1];
+                strcpy(this->lname, lname);
+            }
+            else
+                this->lname = nullptr;
+            abbereviation = false;
+        }
+    }
+    
+    Name::Name(const char* fname)
+    {
+        if (fname != nullptr){
+            this->fname = new char[strlen(fname) + 1];
+            strcpy(this->fname, fname);
+            this->mname = nullptr;
+            this->lname = nullptr;
+            abbereviation = false;
+        }          
+        else
+            setEmpty();
+    }
+    
+    Name::~Name()
+    {
+        delete[] fname;
+        delete[] mname;
+        delete[] lname;
+    }
+    
+    Name& Name::operator=(const Name& N)
+    {
+        if (this != &N){
+            delete[] fname;
+            delete[] mname;
+            delete[] lname;
+            if (N.fname != nullptr && !checkSpace(N.fname)){
+                fname = new char[strlen(N.fname) + 1];
+                strcpy(fname, N.fname);
+            }
+            else
+                fname = nullptr;
+            if (N.mname != nullptr && !checkSpace(N.mname)){
+                mname = new char[strlen(N.mname) + 1];
+                strcpy(mname, N.mname);
+            }
+            else
+                mname = nullptr;
+            if (N.lname != nullptr && !checkSpace(N.lname)){
+                lname = new char[strlen(N.lname) + 1];
+                strcpy(lname, N.lname);
+            }
+            else
+                lname = nullptr;
+        }
+        return *this;
+    }
 
-	bool sdds::Name::isvalid(const char* name)//needs to be worked on
-	{
-		bool result= true;
-		if (name == nullptr || name == " " || name == "") result = false;
-		return result;
-	}
+    void Name::setEmpty()
+    {
+        delete[] fname;
+        delete[] mname;
+        delete[] lname;
+        fname = nullptr;
+        mname = nullptr;
+        lname = nullptr;
+        abbereviation = false;
+    }
+    
+    void Name::setShort(bool abber){
+        this->abbereviation = abber;
+    }
+    
+    Name& Name::operator+=(const char* str){
+        if (str != nullptr && !checkSpace(str)){
+            if (fname == nullptr){
+                fname = new char[strlen(str) + 1];
+                strcpy(fname, str);
+            }
+            else if (mname == nullptr){
+                mname = new char[strlen(str) + 1];
+                strcpy(mname, str);
+            }
+            else if (lname == nullptr){
+                lname = new char[strlen(str) + 1];
+                strcpy(lname, str);
+            }
+            else
+                setEmpty();
+        }
+        return *this;
+    }
 
-	void Name::set(const char* fname, const char* lname, const char* mname)
-	{
-		if (isvalid(fname)) {
-			m_fname = new char[strlen(fname) + 1];
-			strcpy(m_fname, fname);
-		}
-		if (isvalid(lname)) {
-			m_lname = new char[strlen(lname) + 1];
-			strcpy(m_lname, lname);
-		}
-		if (isvalid(mname)) {
-			m_mname = new char[strlen(mname) + 1];
-			strcpy(m_mname, mname);
-		}
-	}
+    Name::operator bool() const {
+        return isvalid();
+    }
 
-	sdds::Name::Name(const char* fname, const char* lname, const char* mname)
-	{
-		setEmpty();
-			set(fname, lname, mname);
-		
-	}
-	Name::Name(const Name& N)
-	{
-		setEmpty();
-		set(N.m_fname, N.m_lname, N.m_mname);
-	}
-	Name& Name::operator=(const Name& N)
-	{
-		setEmpty();
-		if (this != &N) {
-			set(N.m_fname, N.m_lname, N.m_mname);			
-		}
-		return *this;
-	}
-	Name::~Name()
-	{
-		delete[] m_fname;
-		delete[] m_lname;
-		delete[] m_mname;
-	}
+    const char* Name::getf() const {
+        return fname;
+    }
+
+    const char* Name::getm() const {
+        return mname;
+    }
+
+    const char* Name::getl() const {
+        return lname;
+    }
+
+    bool Name::isvalid() const{
+        bool result = true;
+        if (fname == nullptr) result = false;
+            return result;
+    }
+
+    bool Name::isShort() const{
+        return abbereviation;
+    }
+
+    istream& operator>>(istream& istr, Name& name)
+    {
+        char f_name[MAX_NAME_LEN];
+        char m_name[MAX_NAME_LEN];
+        char l_name[MAX_NAME_LEN];
+        char c;
+        name.setEmpty();
+        istr >> f_name;
+        if (istr.fail())
+            return istr;
+        istr.get(c);
+        if (c == '\n') {
+            name = Name(f_name);
+            return istr;
+        }
+        istr >> m_name;
+        if (istr.fail()) {
+            istr.clear();
+            istr.ignore(MAX_NAME_LEN, '\n');
+            return istr;
+        }
+        istr.get(c);
+        if (c == '\n') {
+            name = Name(f_name, m_name);
+            return istr;
+        }
+        istr >> l_name;
+        if (istr.fail()) {
+            istr.clear();
+            istr.ignore(MAX_NAME_LEN, '\n');
+            return istr;
+        }
+        istr.get(c);
+        if (istr.fail()) {
+            istr.clear();
+            istr.ignore(MAX_NAME_LEN, '\n');
+            return istr;
+        }
+        if (c != '\n') {
+            name.setEmpty();
+            return istr;
+        }
+        name = Name(f_name, m_name, l_name);
+        return istr;
+    }
+
+    ostream& operator<<(ostream& ostr, const Name& N) {
+        if (N.isvalid()) {
+            if (N.isShort()) {
+                ostr << N.getf();
+                if (N.getm() != nullptr && strlen(N.getm()) > 0)
+                    ostr << ' ' << N.getm()[0] << ".";
+                if (N.getl() != nullptr && strlen(N.getl()) > 0)
+                    ostr << " " << N.getl();
+            }
+            else {
+                ostr << N.getf();
+                if (N.getm() != nullptr && strlen(N.getm()) > 0)
+                    ostr << " " << N.getm();
+                if (N.getl() != nullptr && strlen(N.getl()) > 0)
+                    ostr << " " << N.getl();
+            }
+        }
+        else
+            ostr << "Bad Name";
+        return ostr;
+    }
 }
--- Workshop6Part2/Name.h
+++ Workshop6Part2/Name.h
@@ -2,34 +2,42 @@
 Name: Ashmeet Kaur
 Seneca email: ashmeet-kaur1@myseneca.ca
 Student Id: 122421217
-Date: March 06, 2022
+Date: March 07, 2022
 I have done all the coding by myself and only copied the code that my professor provided to
  complete my workshops and assignments.
 */
 #ifndef SDDS_NAME_H
 #define SDDS_NAME_H
-#include <iostream>;
-namespace sdds {
-	class Name {
-		char* m_fname{};
-		char* m_mname{};
-		char* m_lname{};
-		void setEmpty();
-		bool isvalid(const char* name);
-		void set(const char* fname, const char* lname, const char* mname);
-	public:
-		Name(const char* fname = nullptr, const char* lname = nullptr, const char* mname = nullptr);
-		Name(const Name& N);
-		Name& operator=(const Name& N);
-		~Name();
-		
-		void setShort(bool arg = false);
-		Name& operator+=(const Name& N);
-		operator bool() const;
-		std::istream& read(std::istream& istr);
-	};
-	char operator+=(Name& N, const char* name);
-	std::istream& operator>>(std::istream& istr, Name& N);
-	std::ostream& operator<<(std::ostream& ostr, const Name& N);
+#include <iostream>
+#include <cstring>
+namespace sdds
+{
+    class Name{
+    private:
+        char* fname;
+        char* mname;
+        char* lname;
+        bool abbereviation;       
+    public:
+        Name();
+        Name(const char* fname, const char* mname , const char* lname);
+        Name(const Name& N);
+        Name(const char* fname, const char* lname);
+        Name(const char* fname);
+        ~Name();
+        void setEmpty();
+        Name& operator=(const Name& N);
+        void setShort(bool abber);
+        Name& operator+=(const char* str);
+        const char* getf() const;
+        const char* getm() const;
+        const char* getl() const;
+        operator bool() const;
+        bool checkSpace(const char* str);
+        bool isvalid() const;
+        bool isShort() const;
+    };
+    std::istream& operator>>(std::istream& istr, Name& name);
+    std::ostream& operator<<(std::ostream& ostr, const Name& name);
 }
 #endif // !SDDS_NAME_H
