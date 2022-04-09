56b2fbd948c3c8c3814c65861047b2053a5f1656
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2022-04-09 05:04:23 -0400

    workshop updated: copying problem

--- workshop10/temps.h
+++ workshop10/temps.h
@@ -1,6 +1,29 @@
 #ifndef SDDS_TEMPS_H
 #define SDDS_TEMPS_H
+#include "Collection.h"
+#include <iostream>
 namespace sdds {
+	template <typename T>
+	Collection<T> select(const T arr[], int a, const T& A) {
+		int count = 0;
+		for ( int i = 0; i < a; i++) {
+			if (arr[i] == A) { count++;// std::cout << arr[i] << std::endl;
+			}
+		}
+		Collection<T> s = Collection<T>(count);
+		for ( int i = 0; i < a; i++) {
+			if (arr[i] == A) s[i] = arr[i];
+		}
+		return s;
+	}
 
+	template <typename T>
+	void printCollection(const Collection<T>& A, const char* prompt) {
+		std::cout << prompt << std::endl;
+		for (int i = 0; i < A.size(); i++) {
+			std::cout << A[i] << std::endl;
+		}
+	}
+	//void sort(T arr[], int n, bool Ascending = true)
 }
 #endif // !SDDS_TEMPS_H
