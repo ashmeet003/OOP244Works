f59a1ed7510a6f277e92a897ff052b09dbd8f80b
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2022-04-13 19:56:41 -0400

    Workshop10 temps.h edited:Comments added

--- workshop10/temps.h
+++ workshop10/temps.h
@@ -3,12 +3,15 @@
 #include "Collection.h"
 #include <iostream>
 namespace sdds {
+	/*The template for select() needed oerator== for respective type to compare values.
+	It also needed constructor to assign the size to new object of type Collection<T>
+	Futhermore, the function needs a assignment operator to copy values from the given array to new array
+	and also operator[] to access the values*/
 	template <typename T>
 	Collection<T> select(const T arr[], int a, const T& A) {
 		int count = 0;
 		for ( int i = 0; i < a; i++) {
-			if (arr[i] == A) { count++;// std::cout << arr[i] << std::endl;
-			}
+			if (arr[i] == A) count++;			
 		}
 		unsigned int j = 0;
 		Collection<T> s = Collection<T>(count);
@@ -17,7 +20,8 @@ namespace sdds {
 		}
 		return s;
 	}
-
+	/*The template for printCollection needed accessor method to get the size of the array and operator[] to access values
+	Finally, the values are displayed usinf display() for each type*/
 	template <typename T>
 	void printCollection(const Collection<T>& A, const char* prompt) {
 		std::cout << prompt << std::endl;
@@ -25,6 +29,5 @@ namespace sdds {
 			std::cout << A[i] << std::endl;
 		}
 	}
-	//void sort(T arr[], int n, bool Ascending = true)
 }
 #endif // !SDDS_TEMPS_H
